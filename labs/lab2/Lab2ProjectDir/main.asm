7f324c7feda9d829da2c71f774801c6904840e99
abpwrs <alexander-powers@uiowa.edu>
2019-02-07 10:38:04 -0600

    display subroutines for 0-9A-F

--- labs/lab2/Lab2ProjectDir/main.asm
+++ labs/lab2/Lab2ProjectDir/main.asm
@@ -16,6 +16,7 @@
 .EQU SER_IN = 0
 .EQU SRCK = 1
 .EQU RCK = 2
+.EQU MAX_VALUE = 15
 
 .
 
@@ -25,38 +26,117 @@
 ; PB2 = RCK
 
 ; Configure PB0, PB1, and PB2 as output pins.
-    sbi DDRB,0;
+    sbi DDRB,0
     sbi DDRB,1
     sbi DDRB,2
 	sbi DDRB,3
 	sbi DDRB,4
 ;
   main:
-	sbi PORTB, 3
-	sbi PORTB, 4
-    sbi PORTB,SER_IN
+;	sbi PORTB, 3
+;	sbi PORTB, 4
+;    sbi PORTB,SER_IN
+;	rcall delay_long
+;
+;	sbi PORTB,SRCK
+;	rcall delay_long
+;
+;	cbi PORTB,SRCK
+;	rcall delay_long
+;
+;	sbi PORTB,RCK
+;	rcall delay_long
+;
+;	cbi PORTB, RCK
+;	rcall delay_long
+;
+;	cbi PORTB,SER_IN
+;	rcall delay_long
+
+;	rcall shift_one_on
+;	rcall shift_one_on
+;	rcall reset_disp
+;	rcall delay_long
+;	rcall shift_one_off
+;	rcall shift_one_off
+;	rcall reset_disp
+;	rcall delay_long
+
+	rcall disp_zero
 	rcall delay_long
 
-	sbi PORTB,SRCK
+	rcall disp_one
 	rcall delay_long
 
-	cbi PORTB,SRCK
+	rcall disp_two
 	rcall delay_long
 
-	sbi PORTB,RCK
+	rcall disp_three
 	rcall delay_long
 
-	cbi PORTB, RCK
+	rcall disp_four
 	rcall delay_long
 
-	cbi PORTB,SER_IN
+	rcall disp_five
+	rcall delay_long
+
+	rcall disp_six
+	rcall delay_long
+
+	rcall disp_seven
+	rcall delay_long
+
+	rcall disp_eight
+	rcall delay_long
+
+	rcall disp_nine
+	rcall delay_long
+
+	rcall disp_a
+	rcall delay_long
+
+	rcall disp_b
+	rcall delay_long
+
+	rcall disp_c
+	rcall delay_long
+
+	rcall disp_d
+	rcall delay_long
+
+	rcall disp_e
+	rcall delay_long
+
+	rcall disp_f
 	rcall delay_long
 
 	rjmp main
 	
+shift_one_on:
+	sbi PORTB, SER_IN
+	nop
+	sbi PORTB, SRCK
+	nop
+	cbi PORTB, SRCK
+	ret
+
+shift_one_off:
+	cbi PORTB, SER_IN
+	nop
+	sbi PORTB, SRCK
+	nop
+	cbi PORTB, SRCK
+	ret
+
+reset_disp:
+	sbi PORTB, RCK
+	nop
+	cbi PORTB, RCK
+	ret
     
-   delay_long:
-      ldi   r23,11      ; r23 <-- Counter for outer loop
+    
+delay_long:
+      ldi   r23,20      ; r23 <-- Counter for outer loop
   d1: ldi   r24,226     ; r24 <-- Counter for level 2 loop 
   d2: ldi   r25,249     ; r25 <-- Counter for inner loop
   d3: dec   r25
@@ -67,5 +147,215 @@
       dec   r23
       brne  d1
       ret
+
+; 0-1-1-1-0-1-1-1
+disp_zero:
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+
+; 0-0-0-0-0-1-1-0
+disp_one:
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall reset_disp
+	ret
+
+; 1-0-1-1-0-0-1-1
+disp_two:
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+;1-0-0-1-0-1-1-1
+disp_three:
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-0-0-0-1-1-0
+disp_four:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall reset_disp
+	ret
+
+; 1-1-0-1-0-1-0-1
+disp_five:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-1-1-0-1-0-1
+disp_six:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 0-0-0-0-0-1-1-1
+disp_seven:
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-1-1-0-1-1-1
+disp_eight:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-0-1-0-1-1-1
+disp_nine:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-1-0-0-1-1-1
+disp_a:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-1-1-0-1-0-0
+disp_b:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall reset_disp
+	ret
+
+; 0-1-1-1-0-0-0-1
+; 0-1-1-1-0-0-0-1
+disp_c:
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-0-1-1-0-1-1-0
+disp_d:
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall reset_disp
+	ret
+
+ ;1-1-1-1-0-0-0-1
+disp_e:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall reset_disp
+	ret
+
+; 1-1-1-0-0-0-0-1
+disp_f:
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_on
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_off
+	rcall shift_one_on
+	rcall reset_disp
+	ret
 
 .exit
