bb31f63ff927ab63d836f4f9cf9fed8c73acf6d6
abpwrs <alexander-powers@uiowa.edu>
2019-02-11 15:37:11 -0600

    adding control flow for button timing

--- labs/lab2/Lab2ProjectDir/main.asm
+++ labs/lab2/Lab2ProjectDir/main.asm
@@ -45,6 +45,7 @@ main:
 nop
 sbis PINB, PUSH_BUTTON ; if button pushed, next line will execute
 rcall button_pressed ; react to the button press
+rcall reset_disp
 rjmp main
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
@@ -57,8 +58,11 @@ ret
 ; count press sets certain register values associated with the length of button press
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 count_press:
-
-
+rcall my_delay
+cpi R18, 255
+breq skip
+inc R18
+skip:
 sbis PINB, PUSH_BUTTON ; if button is still pressed, execute next line
 rjmp count_press ; rjmp to this method
 ret
@@ -67,7 +71,7 @@ ret
 ; my delay of 10000 cycles -- currently ~ 10032 cycles
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 my_delay:
-         ldi   r23,3      ; r23 <-- Counter for outer loop
+         ldi   r23,2      ; r23 <-- Counter for outer loop
   my_d1: ldi   r24,20     ; r24 <-- Counter for level 2 loop 
   my_d2: ldi   r25,41      ; r25 <-- Counter for inner loop
   my_d3: dec   r25
@@ -83,9 +87,54 @@ my_delay:
 ; update the state based on the contents of R17
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 update:
-nop
+cpi R18, 0xc8
+brge update_one
+
+cpi R18, 0x64
+brge update_two
+
+rjmp update_three
+
+update_one:
+rcall reset_routine
+rjmp update_end
+
+update_two:
+rcall toggle_routine
+rjmp update_end
+
+update_three:
+rcall move_routine
+rjmp update_end
+
+
+update_end:
 ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+reset_routine:
+nop
+ldi R16, ZERO_DISP
+rcall display
+rcall delay_long
+ret 
+
+
+toggle_routine:
+nop
+ldi R16,  ONE_DISP
+rcall display
+rcall delay_long
+ret
+
+
+
+move_routine:
+nop
+ldi R16, TWO_DISP
+rcall display
+rcall delay_long
+ret
 
 
 
@@ -133,7 +182,7 @@ ret
 ; subroutine for delay -- this isn't used in the final product
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 delay_long:
-      ldi   r23,2      ; r23 <-- Counter for outer loop
+      ldi   r23,20      ; r23 <-- Counter for outer loop
   d1: ldi   r24,200     ; r24 <-- Counter for level 2 loop 
   d2: ldi   r25,200     ; r25 <-- Counter for inner loop
   d3: dec   r25
@@ -199,6 +248,12 @@ rcall display
 rcall delay_long
 ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+reset_disp:
+ldi R16, 0x00
+rcall display
+ret
 
 ; end of program -- should never be reached due to main infinite loop
 .exit
