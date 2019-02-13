bb23fed9a54f28ab040bfc860ac0e54a7397c88b
abpwrs <alexander-powers@uiowa.edu>
2019-02-13 11:28:18 -0600

    final version of lab2

--- labs/lab2/Lab2ProjectDir/main.asm
+++ labs/lab2/Lab2ProjectDir/main.asm
@@ -3,64 +3,55 @@
 ; Assembly language file for Lab 2 in ECE:3360 (Embedded Systems)
 ; Spring 2018, The University of Iowa.
 ;
-; Desc: runs the control logic for a buttone controlled counter
+; Desc: runs the control logic for a button controlled counter
 ;
 ; B. Mitchinson, A. Powers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; TODO: Revise all comments
-; TODO: Replace r30 with z low. Just Z? Z high doesn't have usage at
-; this point.
 
 .include "tn45def.inc"
-.include "disp_values.inc"
+.include "disp_values.inc" ; file with hex constants for 7-segment display
 
 .dseg
 .org 0x0060 ;Tiny45, Start in SRAM
-;disp_table: .db 0x77, 0x06, 0xB3, 0x97, 0xc6, 0xd5, 0xf5, 0x07, 0xf7, 0xd7, 0xe7, 0xf4, 0x71, 0xb6, 0xf1, 0xe1
 disp_table: .byte 16
 
 .cseg
 .org 0x0000
 
-; test initialize SRAM
-; TODO: Can I move this out to a different file? Can disp_table be a db like the original goal?
-; During 2/12 lecture he advised to move it to cseg as db since it's static right?
-ldi r20, 0x77
+; initialize SRAM
+ldi r20, ZERO_DISP
 sts disp_table, r20
-ldi r20, 0x06
+ldi r20, ONE_DISP
 sts disp_table+1, r20
-ldi r20, 0xB3
+ldi r20, TWO_DISP
 sts disp_table+2, r20
-ldi r20, 0x97
+ldi r20, THREE_DISP
 sts disp_table+3, r20
-ldi r20, 0xc6
+ldi r20, FOUR_DISP
 sts disp_table+4, r20
-ldi r20, 0xd5
+ldi r20, FIVE_DISP
 sts disp_table+5, r20
-ldi r20, 0xf5
+ldi r20, SIX_DISP
 sts disp_table+6, r20
-ldi r20, 0x07
+ldi r20, SEVEN_DISP
 sts disp_table+7, r20
-ldi r20, 0xf7
+ldi r20, EIGHT_DISP
 sts disp_table+8, r20
-ldi r20, 0xd7
+ldi r20, NINE_DISP
 sts disp_table+9, r20
-ldi r20, 0xe7
+ldi r20, A_DISP
 sts disp_table+10, r20
-ldi r20, 0xf4
+ldi r20, B_DISP
 sts disp_table+11, r20
-ldi r20, 0x71
+ldi r20, C_DISP
 sts disp_table+12, r20
-ldi r20, 0xb6
+ldi r20, D_DISP
 sts disp_table+13, r20
-ldi r20, 0xf1
+ldi r20, E_DISP
 sts disp_table+14, r20
-ldi r20, 0xe1
+ldi r20, F_DISP
 sts disp_table+15, r20
 clr r20
-
-; test
-ldi r20, 95
 
 ; 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -78,12 +69,13 @@ ldi r20, 95
 ; PB0 = SER_IN
 ; PB1 = SRCK
 ; PB2 = RCK
+; PB4 = button input 
 ; Configure PB0, PB1, and PB2 as output pins.
+; Configure PB4 as input pins
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 sbi DDRB,0
 sbi DDRB,1
 sbi DDRB,2
-cbi DDRB,3
 cbi DDRB,4
 
 ldi ZL, low(disp_table) ;r30
@@ -94,6 +86,7 @@ ldi ZH, high(disp_table) ;r31
 ; main method -- infinite loop to keep the controller responding to input
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 rcall reset_routine
+
 main:
 nop
 sbis PINB, PUSH_BUTTON ; if button pushed, next line will execute
@@ -102,7 +95,7 @@ rjmp main
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
 button_pressed:
-ldi PRESS_TIME_REG, 0x00 ; set initial state of counter to zero
+clr PRESS_TIME_REG ; set initial state of counter to zero
 rcall count_press
 rjmp update
 
@@ -123,8 +116,7 @@ rjmp count_press
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; TODO: Update with proper desmos calc: 
-; my delay of 10000 cycles -- currently ~ 10032 cycles
+; sample_delay of 80000 cycles 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 sample_delay:
          ldi   r23,20      ; r23 <-- Counter for outer loop
@@ -143,10 +135,6 @@ sample_delay:
 ; update the state based on the contents of PRESS_TIME_REG
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 update:
-
-; TEST CASE
-;add PRESS_TIME_REG, r20
-
 cpi PRESS_TIME_REG, 200
 brsh reset_routine
 
@@ -155,13 +143,11 @@ brsh toggle_routine
 
 rjmp move_routine
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 reset_routine:
-nop
-;;;;;;;;;;;;;;; TODO: CHANGE LOOKUP TABLE PTR TO ZERO
+nop 
 ldi ZL, 0x60
 ld DISP_REG, Z
-rcall display
+rcall toggle_dec_off
 rjmp main
 
 toggle_routine:
@@ -170,7 +156,7 @@ cpi DEC_REG, 0x00
 breq toggle_dec_on
 cpi DEC_REG, 0x00
 brne toggle_dec_off
-rjmp main ;saftey. could remove, but just incase.
+rjmp main 
 
 move_routine:
 nop
@@ -187,8 +173,6 @@ cpi DEC_REG, 0x00
 brne neg_counter
 rjmp main ; saftey
 
-; Is this really the best way to do nested conditionals? 
-; dedicated sub routines?
 neg_overflow:
 cpi DEC_REG, 0x01
 breq bottom_overflow
@@ -215,7 +199,7 @@ ld DISP_REG, Z
 rcall display
 rjmp main
 
-bottom_overflow: ; inverse "reset"
+bottom_overflow:
 ldi ZL, 0x6F
 ld DISP_REG, Z
 rcall display
@@ -284,76 +268,6 @@ out SREG, DISP_INS_REG
 pop DISP_INS_REG
 pop DISP_REG
 ret  
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; subroutine for delay -- this isn't used in the final product
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-delay_long:
-      ldi   r23,20      ; r23 <-- Counter for outer loop
-  d1: ldi   r24,200     ; r24 <-- Counter for level 2 loop 
-  d2: ldi   r25,200     ; r25 <-- Counter for inner loop
-  d3: dec   r25
-      nop              ; no operation 
-      brne  d3 
-      dec   r24
-      brne  d2
-      dec   r23
-      brne  d1
-      ret
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; subroutine to print out all digits
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-print_all:
-ldi DISP_REG, ZERO_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, ONE_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, TWO_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, THREE_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, FOUR_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, FIVE_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, SIX_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, SEVEN_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, EIGHT_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, NINE_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, A_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, B_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, C_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, D_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, E_DISP
-rcall display
-rcall delay_long
-ldi DISP_REG, F_DISP
-rcall display
-rcall delay_long
-ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; end of program -- should never be reached due to main infinite loop
