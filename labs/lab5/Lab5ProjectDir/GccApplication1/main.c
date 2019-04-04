1913ac67c24e41fc56abc9a9ec0814225bd19094
Ben Mitchinson <benjamin-mitchinson@uiowa.edu>
2019-04-04 12:29:54 -0500

    Reading from SRAM bug

--- labs/lab5/Lab5ProjectDir/GccApplication1/main.c
+++ labs/lab5/Lab5ProjectDir/GccApplication1/main.c
@@ -23,19 +23,23 @@ void setbaud();
 void transmitenable();
 void sendhi();
 void transmitdisable();
-void usart_prints();
+void usart_prints(const char*);
 void usart_printf();
 // ///////////////////
 
 
-unsigned char tmp;	 // Compiler puts this in SRAM
+//unsigned char tmp;				   // Compiler puts this in SRAM
+//static const char fdata[] PROGMEM = "Flash Gordon\n";  // String in Flash (Storing in flash is broken and idk why? Compile type?)
+const char sdata[] = "Hello World!\n"; // String in SRAM 
+
 int main(void)
 {
 	// DDRC = 0x20;			 // sbi DDRC, 5 : PORTC 5 Output
 	
 	setbaud();
 	transmitenable();
-	sendhi();
+	//sendhi();
+	usart_prints(sdata);
 	//transmitdisable();
 	
     while (1) 
@@ -46,43 +50,43 @@ int main(void)
 
 // Set Asynchronous Normal Mode and configure BAUD Rate @ 9600
 void setbaud(){
-	tmp = UCSR0A;		  // Get mode of operation
-	tmp = tmp & ~(0x02);  // Clear bit 1 to set "Async Normal Mode" (Slide 45 SerialComm)
-	UCSR0A = tmp;		  // Set the mode
-	tmp = 0x33;			  // UBRR0 = [8000000 / 16(9600)] - 1 = 51.083 (51?)
-	UBRR0 = tmp;
+	UCSR0A = UCSR0A & ~(0x02);  // Set the mode to set "Async Normal Mode" (Slide 45 SerialComm)
+	UBRR0 = 0x33;				// UBRR0 = [8000000 / 16(9600)] - 1 = 51.083 (51?)
+	// Wait do I need returns... lol?
 }
 
 // Enable transmit TXEN0 bit in UCSR0b
 void transmitenable(){
-	tmp = UCSR0B;		 
-	tmp = tmp |	0x08;	  // Set TXEN0 (Bit 3) of UCSR0B
-	UCSR0B = tmp;
+	UCSR0B = UCSR0B | 0x08;		// Set TXEN0 (Bit 3) of UCSR0B
 }
 
 // Disable transmit TXEN0 bit in UCSR0b
 void transmitdisable(){
-	tmp = UCSR0B;
-	tmp = tmp &	~(0x08);  // Clear TXEN0 (Bit 3) of UCSR0B
-	UCSR0B = tmp;
+	UCSR0B = UCSR0B & ~(0x08);  // Clear TXEN0 (Bit 3) of UCSR0B
 }
 
 // Load one character into UDR0 for testing transfer
 void sendhi(){
-	tmp = 0x0A; // Newline
-	UDR0 = tmp;
+	UDR0 = 0x0A;	// Newline
 	_delay_ms(20);
-	tmp = 0x0D; // Carriage Return
-	UDR0 = tmp;
+	UDR0 = 0x0D;	// Carriage Return
 	_delay_ms(20);
-	tmp = 0x48; // H
-	UDR0 = tmp;
+	UDR0 = 0x48;	// H
 	_delay_ms(20);
-	tmp = 0x69; // i
-	UDR0 = tmp;
+	UDR0 = 0x69;	// i
 	_delay_ms(20);
-	tmp = 0x21; // !
-	UDR0 = tmp;
-	_delay_ms(20); // Delays do weird stuff based on compile method,
-				   //    so be careful. (Slide 17 + 27, CProgramming)
+	UDR0 = 0x21;	// !
+	_delay_ms(20);  // Delays do weird stuff based on chosen compile method,
+				    //    so be careful. (Slide 17 + 27, CProgramming)
+}
+
+void usart_prints(const char *sdata) {
+	while (*sdata) {
+		// Wait for UDRE0 to become set (==1), which indicates 
+		// the UDR0 is empty and can receive the next character (Slide 46, Serial Comm)
+		while (!(UCSR0A & (1<<UDRE0))){
+		//while (!(UCSR0A & (1<<TXC0))){
+			UDR0 = *(sdata++);
+		}
+	}
 }
