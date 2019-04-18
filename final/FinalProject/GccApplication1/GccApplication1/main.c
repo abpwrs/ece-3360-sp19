4df0fc398430823f46e1b06a943184701b7400b1
abpwrs <alexander-powers@uiowa.edu>
2019-04-18 12:15:15 -0500

    saving work from inclass

--- final/FinalProject/GccApplication1/GccApplication1/main.c
+++ final/FinalProject/GccApplication1/GccApplication1/main.c
@@ -8,19 +8,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// LCD #define statements
+#define LCD_RS  5
+#define LCD_E   3
+#define LCD_D4  0
+#define LCD_D5  1
+#define LCD_D6  2
+#define LCD_D7  3
+#define LED_PORT  5
+#define MORSE_ARR_LEN 6
+
 
 
 
 // GLOBAL
-
-
-
 // Button States
 // UP = 0x01
 // DOWN = 0x00
- char prev_state = 0x00;
- char curr_state = 0x00;
- int  press_len = 0;
+char prev_state = 0x00;
+char curr_state = 0x00;
+int  press_len = 0;
 
 
 // timer that does a thing TBD
@@ -30,32 +37,189 @@
 
 // button press interrupt 
 // all of the logic here
-name(INT0_vect){
-	//if (PORTC & (1<<5)){
-		//PORTC |= (0<<5);
-	//} else {
-		//PORTC |= (1<<5);
-	//}
-	PORTC |= 1 << 5;
-	_delay_ms(1000);
-	PORTC |= 0 << 5;
+button_interrupt_isr(INT0_vect){
+    //if (PORTC & (1<<5)){
+        //PORTC |= (0<<5);
+    //} else {
+        //PORTC |= (1<<5);
+    //}
+    PORTC |= 1 << 5;
+    _delay_ms(1000);
+    PORTC |= 0 << 5;
 
 }
+
 
 
 // prototypes
+// lcd initialization methods
+void lcd_init(void);
+void set_to_8_bit(void);
+void set_to_4_bit(void);
 
+// lcd strobe
+void lcd_strobe(void);
 
+// write char string to lcd
+void lcd_write_char(char *);
+
+// convert 6 integer array
+char morse_to_ascii(int *, size_t);
 
 int main(void)
 {
-	DDRC |= 1 << 5;
-	EICRA |= (1<<ISC00);
-	EIMSK |= (1<<INT0);
-	sei();
-    /* Replace with your application code */
+    // LCD Data Direction Configuration
+    DDRC |= (1<<D4);
+    DDRC |= (1<<D5);
+    DDRC |= (1<<D6);
+    DDRC |= (1<<D7);
+    DDRB |= (1<<RS);
+    DDRB |= (1<<E);
+    
+    // set interrupt configurations
+    DDRC |= 1 << 5;
+    EICRA |= (1<<ISC00);
+    EIMSK |= (1<<INT0);
+
+
+    // enable interrupts
+    sei(void);
+
+    // infinite loop
     while (1) 
     {
+
     }
 }
 
+void lcd_init(void){
+// set to command mode
+    PORTB &= ~(1<<RS);
+// delay 10 ms
+    _delay_ms(10);
+// set to 8 bit
+    send_to_lcd(0x03);
+    send_to_lcd(0x03);
+    send_to_lcd(0x03);
+// set to 4 bit
+    send_to_lcd(0x02);
+    send_to_lcd(0x02);
+// Two rows 5x7 characters
+    send_to_lcd(0x08); 
+// clear display
+    send_to_lcd(0x00); 
+// also clear display
+    send_to_lcd(0x01); 
+// display on, underline + blink off
+    send_to_lcd(0x00); 
+// also display on, underline + blink off 
+    send_to_lcd(0x0c); 
+// display shift off, address increment mode
+    send_to_lcd(0x00); 
+// also display shift off, address increment mode
+    send_to_lcd(0x06); 
+}
+
+// send command does not automatically set to command mode
+void send_to_lcd(char command){
+    // send the command
+    PORTC = command;
+    lcd_strobe();
+    _delay_us(200);
+}
+
+void send_char(char message){
+
+}
+
+
+void lcd_strobe(void){
+    PORTB &= ~(1<<E);
+    _delay_us(200);
+    PORTB |= (1<<E);
+    r_delay_us(200);
+    PORTB &= ~(1<<E);
+}
+
+void lcd_write_char(char * message){
+
+}
+
+char morse_to_ascii(int * morse_arr, size_t used_len){
+    switch (used_len){
+        case 0:
+            // error
+            break;
+
+        case 1: 
+            // lookup table 1
+            break;
+
+        case 2: 
+            // lookup table 2
+            break;
+        case 3: 
+            // lookup table 3
+            break;
+
+        case 4: 
+            // lookup table 4
+            break;
+
+        case 5: 
+            // lookup table 5
+            break;
+
+        case 6: 
+            // lookup table 6
+            break;
+
+        default:
+            // error
+            break;
+    }
+
+}
+
+/*
+ASCII, length 
+45, 1
+54, 1
+41, 2
+49, 2
+4D, 2
+4E, 2
+44, 3
+47, 3
+4B, 3
+4F, 3
+52, 3
+53, 3
+55, 3
+57, 3
+42, 4
+43, 4
+46, 4
+48, 4
+4A, 4
+4C, 4
+50, 4
+51, 4
+56, 4
+58, 4
+59, 4
+5A, 4
+30, 5
+31, 5
+32, 5
+33, 5
+34, 5
+35, 5
+36, 5
+37, 5
+38, 5
+39, 5
+2E, 6
+2C, 6
+3F, 6
+*/
