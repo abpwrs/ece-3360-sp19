bd4c2b6d5d8b91928ceb15488b94a731af72f130
Ben Mitchinson <115bwm@gmail.com>
2019-04-20 18:33:48 -0500

    Comments and extra LCD removal

--- final/FinalProjectDir/FinalProjectDir/main.c
+++ final/FinalProjectDir/FinalProjectDir/main.c
@@ -1,16 +1,28 @@
-/*
- * GccApplication1.c
- *
- * Created: 4/18/2019 10:09:44 AM
- * Author : bmitchinson
- */ 
+// ///////////////////////////////////////////////////////////////////
+// Main C Final Project file for ECE:3360 (Embedded Systems)
+// Spring 2019, The University of Iowa.
+//
+// Desc: Morse Code Transmission Device (Client + Receiver Pair)
+//
+// Authors: B. Mitchinson, A. Powers
+// ///////////////////////////////////////////////////////////////////
 
+// Setting CPU Clock Speed
+#ifndef F_CPU
+#define F_CPU 8000000UL // 8 MHz -> CProgramming Notes, Slide 10
+#endif
+// ///////////////////////
+
+// Imports
+// /////////////////////////////////
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include "lcd.h"
+// /////////////////////////////////
 
 // LCD #define statements
+// /////////////////////////////////
 #define RS  5
 #define E   3
 #define D4  0
@@ -19,52 +31,41 @@
 #define D7  3
 #define LED_PORT  5
 #define MORSE_ARR_LEN 6
+// /////////////////////////////////
 
-
-
-
-// GLOBAL
+// Globals
+// /////////////////////////////////
 // Button States
 // UP = 0x01
 // DOWN = 0x00
 char prev_state = 0x00;
 char curr_state = 0x00;
 int  press_len = 0;
-
+// /////////////////////////////////
 
 // timer that does a thing TBD
 // interrupt HERE
 // reset the timer
 
+// Timer Config
+// /////////////////////////////////
 
-// button press interrupt 
-// all of the logic here
+// /////////////////////////////////
+
+// Interrupt Configuration
+// /////////////////////////////////
+// button press interrupt
 ISR(INT0_vect){
-    if (PORTC & (1<<5)){
-        PORTC &= ~(1<<5);
+    if (PORTC & (1<<5)){  // if off
+        PORTC &= ~(1<<5); // Turn on
     } else {
-       PORTC |= (1<<5);
+       PORTC |= (1<<5);   // Turn off
     }
 }
+// /////////////////////////////////
 
-
-
-// prototypes
-// lcd initialization methods
-
-void set_to_8_bit(void);
-void set_to_4_bit(void);
-
-// lcd strobe
-void lcd_strobe(void);
-
-// write to lcd
-void lcd_write_char(char);
-void lcd_write_str(char *, int);
-
-// convert 6 integer array
-char morse_to_ascii(int *, int);
-
+// Main Loop
+// ///////////////////////////////
 int main(void)
 {
     // LCD Data Direction Configuration
@@ -74,91 +75,37 @@ int main(void)
     DDRC |= (1<<D7);
     DDRB |= (1<<RS);
     DDRB |= (1<<E);
-    //my_lcd_init();
+	
+	// Initial LCD Config
 	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
-	lcd_puts("abc");
-	lcd_putc('B');
 	lcd_home();
-	lcd_putc('Z');
-	lcd_gotoxy(4,1);
-	lcd_putc('Z');
+	//lcd_puts("abc");
+	//lcd_putc('B');
+	//lcd_putc('Z');
+	//lcd_gotoxy(4,1);
+	//lcd_putc('Z');
+	
+	// Turn off the LED to use for button feedback
+	PORTC |= (1<<5);
+	
     // set interrupt configurations
     DDRC |= 1 << 5;
     EICRA |= (1<<ISC00);
     EIMSK |= (1<<INT0);
 
-	// lcd_write_str("TESTING", 7);
     // enable interrupts
     sei();
 
     // infinite loop
     while (1) 
     {
-	    //PORTC |= 1 << 5;
-	    //_delay_ms(1000);
-	    //PORTC &= ~(1<<5);
-	    //_delay_ms(1000);
-		//lcd_write_char('A');
+	    
     }
 }
-
-void my_lcd_init(void){
-// set to command mode
-    PORTB &= ~(1<<RS);
-// delay 10 ms
-    _delay_ms(10);
-// set to 8 bit
-    send_to_lcd(0x03);
-    send_to_lcd(0x03);
-    send_to_lcd(0x03);
-// set to 4 bit
-    send_to_lcd(0x02);
-    send_to_lcd(0x02);
-// Two rows 5x7 characters
-    send_to_lcd(0x08); 
-// clear display
-    send_to_lcd(0x00); 
-// also clear display
-    send_to_lcd(0x01); 
-// display on, underline + blink off
-    send_to_lcd(0x00); 
-// also display on, underline + blink off 
-    send_to_lcd(0x0c); 
-// display shift off, address increment mode
-    send_to_lcd(0x00); 
-// also display shift off, address increment mode
-    send_to_lcd(0x06); 
-}
-
-void lcd_write_char(char character){
-	PORTB |= 1 << RS;
-	character = ((character & 0x0F) << 4 | (character & 0xF0) >> 4);
-	send_to_lcd(character);
-	// swap nibbles
-	character = ((character & 0x0F) << 4 | (character & 0xF0) >> 4);
-	send_to_lcd(character);
-	// swap nibbles back
-}
-
-void lcd_write_str(char * message, int length){
-	for(int i; i<length; ++i){
-		lcd_write_char(message[i]);
-	}
-}
-
-// send command does not automatically set to command mode
-void send_to_lcd(char command){
-    // send the command
-    PORTC = command;
-    lcd_strobe();
-    _delay_us(200);
-}
-
-void send_char(char message){
-
-}
+// ///////////////////////////////
 
 
+// strobe the lcd
 void lcd_strobe(void){
     PORTB &= ~(1<<E);
     _delay_us(200);
@@ -167,7 +114,7 @@ void lcd_strobe(void){
     PORTB &= ~(1<<E);
 }
 
-
+// convert 6 integer array to ascii
 char morse_to_ascii(int * morse_arr, int used_len){
     switch (used_len){
         case 0:
@@ -201,8 +148,16 @@ char morse_to_ascii(int * morse_arr, int used_len){
             // error
             break;
     }
-
 }
+
+/*
+Farnsworth Timing:
+short:             1 unit  (down)
+long:              3 units (down)
+morse-character:   1 unit  (up)
+ascii-character:   3 units (up)
+Word:              7 units or more (up)
+*/
 
 /*
 ASCII, length 
